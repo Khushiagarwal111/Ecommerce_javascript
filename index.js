e2cfb48db5786ba8b1e88e4c891d4f77f7a21bb1
Khushi Agarwal <83951569+Khushiagarwal111@users.noreply.github.com>
2022-09-28 20:27:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+// global imports
+import './src/toggleSidebar.js';
+import './src/cart/toggleCart.js';
+import './src/cart/setupCart.js';
+// specific imports
+import fetchProducts from './src/fetchProducts.js';
+import { setupStore, store } from './src/store.js';
+import display from './src/displayProducts.js';
+import { getElement } from './src/utils.js';
+
+const init = async () => {
+  const products = await fetchProducts();
+  if (products) {
+    // add products to the store
+    setupStore(products);
+    const featured = store.filter((product) => product.featured === true);
+    display(featured, getElement('.featured-center'));
+  }
+};
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+window.addEventListener('DOMContentLoaded', init);
